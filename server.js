73992093856af14bda57e835351b0830c9306839
Nkematu Bonaventure <74823094+ChuloCr8v@users.noreply.github.com>
2025-03-01 08:29:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post("/send-email", async (req, res) => {
 
   // Email options
   const mailOptions = {
-    from: "me", // sender address
+    from: process.env.EMAIL_USER, // sender address
     to, // recipient(s)
     subject, // email subject
     text: phrase, // plain text body (optional)
